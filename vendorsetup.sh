60d05d4b3e47b8d5e6993e5a95e768984ef2f0cf
Manavjit (Kuruin) <80977693+PeacE-Boi@users.noreply.github.com>
2021-09-05 21:22:29 +0530

    Gram: Include vibrator from wave-project

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,7 @@
 # Proton-Clang
 rm -rf prebuilts/clang/host/linux-x86/clang-12
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-12
+
+# Vibrator 
+rm -rf vendor/qcom/opensource/vibrator
+git clone https://github.com/Wave-Project/vendor_qcom_opensource_vibrator -b r vendor/qcom/opensource/vibrator
